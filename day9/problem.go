d1d68f95ba21fefcfeccaf7748694842fe3db1c3
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-08-30 19:10:04 -0600

    day 9 solution. O(N) time and O(1) space

---
+++ day9/problem.go
@@ -0,0 +1,25 @@
+package day9
+
+// MaximumNonAdjacentSum calculates the largest possible sum without using adjacent values.
+func MaximumNonAdjacentSum(nums []int) int {
+	var one, two, result int
+	for i, val := range nums {
+		if i == 0 {
+			result = val
+		} else if i == 1 {
+			result = max(result, val)
+		} else {
+			result = max(one, val+two)
+		}
+		two = one
+		one = result
+	}
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
