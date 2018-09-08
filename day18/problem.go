2a25d963324c835c39095916cf27dfbc423181c5
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-09-08 12:03:55 -0600

    day 18 implement a one pass version

--- day18/problem.go
+++ day18/problem.go
@@ -14,3 +14,22 @@ func MaxSubarraysBrute(nums []int, k int) []int {
 	}
 	return result
 }
+
+// MaxSubarraysOnePass runs through the input only once.
+func MaxSubarraysOnePass(nums []int, k int) []int {
+	var result []int
+	var sublist []int // O(k) extra space
+	for i := 0; i < len(nums); i++ {
+		if i >= k {
+			result = append(result, nums[sublist[0]])
+		}
+		for len(sublist) > 0 && sublist[0] <= i-k {
+			sublist = sublist[1:]
+		}
+		for len(sublist) > 0 && nums[i] >= nums[sublist[len(sublist)-1]] {
+			sublist = sublist[:len(sublist)-1]
+		}
+		sublist = append(sublist, i)
+	}
+	return append(result, nums[sublist[0]])
+}
--- day18/problem_test.go
+++ day18/problem_test.go
@@ -29,3 +29,20 @@ func BenchmarkMaxSubarraysBrute(b *testing.B) {
 		}
 	}
 }
+
+func TestMaxSubarraysOnePass(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := MaxSubarraysOnePass(tc.input, tc.k); !reflect.DeepEqual(tc.expected, result) {
+			t.Errorf("Expected %v bot got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkMaxSubarraysOnePass(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MaxSubarraysOnePass(tc.input, tc.k)
+		}
+	}
+}
