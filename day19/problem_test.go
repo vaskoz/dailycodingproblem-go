a7166de953c55cd0b75ecee944f51274db2d1830
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-09-09 14:57:10 -0600

    day 19 basic dynamic programming

---
+++ day19/problem_test.go
@@ -0,0 +1,26 @@
+package day19
+
+import "testing"
+
+var testcases = []struct {
+	houses   Houses
+	expected int
+}{
+	{Houses{PaintingCosts{2, 4, 5}, PaintingCosts{3, 6, 9}}, 7},
+}
+
+func TestMinPaintingCost(t *testing.T) {
+	for _, tc := range testcases {
+		if result := MinPaintingCost(tc.houses); result != tc.expected {
+			t.Errorf("Expected %v but got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkMinPaintingCost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MinPaintingCost(tc.houses)
+		}
+	}
+}
