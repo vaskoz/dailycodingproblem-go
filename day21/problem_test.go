680371ff09ce0db782b890c246796d388ad0c4aa
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-09-11 14:56:43 -0600

    day 21 - rooms for classes

---
+++ day21/problem_test.go
@@ -0,0 +1,28 @@
+package day21
+
+import "testing"
+
+var testcases = []struct {
+	lectures    []Lecture
+	roomsNeeded int
+}{
+	{[]Lecture{{30, 75}, {0, 50}, {60, 150}}, 2},
+	{[]Lecture{{0, 10}, {10, 20}, {20, 30}, {1, 11}, {11, 21}, {21, 31}}, 2},
+}
+
+func TestMinimumRooms(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := MinimumRooms(tc.lectures); result != tc.roomsNeeded {
+			t.Errorf("Expected %v but got %v", tc.roomsNeeded, result)
+		}
+	}
+}
+
+func BenchMarkMinimumRooms(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MinimumRooms(tc.lectures)
+		}
+	}
+}
