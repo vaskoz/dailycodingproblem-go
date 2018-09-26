2b7bc2552e40c18df5b82fd00e2395e57feb3170
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-09-25 23:18:13 -0400

    day 35 solution

---
+++ day35/problem.go
@@ -0,0 +1,20 @@
+package day35
+
+// SegregateValues takes a slice of letters and segregates them in-place.
+// O(N) time and O(1) space.
+func SegregateValues(letters []rune) {
+	var ri, gi int
+	for i, r := range letters {
+		if r == 'R' {
+			letters[i] = letters[gi]
+			letters[gi] = letters[ri]
+			letters[ri] = r
+			gi++
+			ri++
+		} else if r == 'G' {
+			letters[i] = letters[gi]
+			letters[gi] = r
+			gi++
+		}
+	}
+}
