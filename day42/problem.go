804f2345733442ff1debbd8bc0a7ad77025d8e98
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-10-02 21:38:36 -0600

    day 42 - added psuedo-polynomial time dp solution

--- day42/problem.go
+++ day42/problem.go
@@ -15,3 +15,29 @@ func SubsetSumBrute(ints []int, k int) []int {
 	}
 	return nil
 }
+
+// SubsetSumDP returns the subset using dynamic programming.
+// The runtime is O(N*K) with O(N*K) space.
+func SubsetSumDP(ints []int, k int) []int {
+	subsets := make([][][]int, len(ints)+1)
+
+	for i := 0; i <= len(ints); i++ {
+		subsets[i] = make([][]int, k+1)
+		subsets[i][0] = []int{}
+	}
+
+	for i := 1; i <= k; i++ {
+		subsets[0][i] = nil
+	}
+
+	for i := 1; i <= len(ints); i++ {
+		for j := 1; j <= k; j++ {
+			subsets[i][j] = subsets[i-1][j]
+			if j >= ints[i-1] && subsets[i-1][j-ints[i-1]] != nil {
+				subsets[i][j] = append([]int{ints[i-1]}, subsets[i-1][j-ints[i-1]]...)
+			}
+		}
+	}
+
+	return subsets[len(ints)][k]
+}
--- day42/problem_test.go
+++ day42/problem_test.go
@@ -2,6 +2,7 @@ package day42
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -10,14 +11,18 @@ var testcases = []struct {
 	k        int
 	expected []int
 }{
-	{[]int{12, 1, 61, 5, 9, 2}, 24, []int{12, 1, 9, 2}},
+	{[]int{12, 1, 61, 5, 9, 2}, 24, []int{1, 2, 9, 12}},
 	{[]int{12, 1, 61, 5, 9, 2}, 1000, nil},
+	{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+		1000, nil},
 }
 
 func TestSubsetSumBrute(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testcases {
-		if result := SubsetSumBrute(tc.input, tc.k); !reflect.DeepEqual(tc.expected, result) {
+		result := SubsetSumBrute(tc.input, tc.k)
+		sort.Ints(result)
+		if !reflect.DeepEqual(tc.expected, result) {
 			t.Errorf("Expected %v got %v", tc.expected, result)
 		}
 	}
@@ -27,6 +32,25 @@ func BenchmarkSubsetSumBrute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			SubsetSumBrute(tc.input, tc.k)
+		}
+	}
+}
+
+func TestSubsetSumDP(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		result := SubsetSumDP(tc.input, tc.k)
+		sort.Ints(result)
+		if !reflect.DeepEqual(tc.expected, result) {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkSubsetSumDP(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			SubsetSumDP(tc.input, tc.k)
 		}
 	}
 }
