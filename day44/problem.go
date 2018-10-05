0ab7bc9c2c76812169e5aee0a3474a63025a83ca
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-10-04 21:11:00 -0600

    day 44 - n log n solution for counting inversions

--- day44/problem.go
+++ day44/problem.go
@@ -17,5 +17,51 @@ func InversionCountBrute(array []int) int {
 // InversionCount returns the inversion count.
 // Runtime is O(N log N).
 func InversionCount(array []int) int {
-	return 0
+	copied := make([]int, len(array))
+	copy(copied, array)
+	return mergeSort(copied, make([]int, len(array)), 0, len(array)-1)
+}
+
+func mergeSort(input, temp []int, left, right int) int {
+	var count int
+	if left < right {
+		mid := (left + right) / 2
+		count = mergeSort(input, temp, left, mid)
+		count += mergeSort(input, temp, mid+1, right)
+		count += merge(input, temp, left, mid+1, right)
+	}
+	return count
+}
+
+func merge(input, temp []int, left, mid, right int) int {
+	li, ri, resi, count := left, mid, left, 0
+	for (li <= mid-1) && (ri <= right) {
+		if input[li] <= input[ri] {
+			temp[resi] = input[li]
+			resi++
+			li++
+		} else {
+			temp[resi] = input[ri]
+			resi++
+			ri++
+			count += (mid - li)
+		}
+	}
+	for li <= mid-1 {
+		temp[resi] = input[li]
+		resi++
+		li++
+	}
+
+	for ri <= right {
+		temp[resi] = input[ri]
+		resi++
+		ri++
+	}
+
+	for i := left; i <= right; i++ {
+		input[i] = temp[i]
+	}
+
+	return count
 }
