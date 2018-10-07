00670f1970af1fcd7b5f430bed1524a77d6e2d3c
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-10-07 12:45:36 -0600

    day 47 - add linear time solution for max profit

--- day47/problem.go
+++ day47/problem.go
@@ -13,3 +13,23 @@ func MaxProfitBrute(stockPrices []int) int {
 	}
 	return maxProfit
 }
+
+// MaxProfit calculates the maximum profit from these chronological stock prices.
+// Runs in O(N) time and O(1) space.
+func MaxProfit(stockPrices []int) int {
+	if len(stockPrices) == 0 {
+		return 0
+	}
+	var maxProfit, lowestPrice int
+	lowestPrice = stockPrices[0]
+	for _, price := range stockPrices {
+		if price < lowestPrice {
+			lowestPrice = price
+		} else {
+			if profit := price - lowestPrice; profit > maxProfit {
+				maxProfit = profit
+			}
+		}
+	}
+	return maxProfit
+}
--- day47/problem_test.go
+++ day47/problem_test.go
@@ -7,6 +7,8 @@ var testcases = []struct {
 	maxProfit int
 }{
 	{[]int{9, 11, 8, 5, 7, 10}, 5},
+	{[]int{}, 0},
+	{nil, 0},
 }
 
 func TestMaxProfitBrute(t *testing.T) {
@@ -22,6 +24,23 @@ func BenchmarkMaxProfitBrute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			MaxProfitBrute(tc.prices)
+		}
+	}
+}
+
+func TestMaxProfit(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if profit := MaxProfit(tc.prices); tc.maxProfit != profit {
+			t.Errorf("Expected profit %d but got %d", tc.maxProfit, profit)
+		}
+	}
+}
+
+func BenchmarkMaxProfit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MaxProfit(tc.prices)
 		}
 	}
 }
