a78d95b7a69bf503cf15878e4a3254dc6b125c4b
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-10-13 22:22:12 -0600

    day 48 - reconstruct a binary tree from a pre-order and in-order representation

---
+++ day48/problem.go
@@ -0,0 +1,32 @@
+package day48
+
+// BinaryTree is a binary tree with rune values.
+type BinaryTree struct {
+	value       string
+	left, right *BinaryTree
+}
+
+// Reconstruct takes a pre-order and in-order string representation
+// and then returns a BinaryTree that conforms to both of those representations.
+func Reconstruct(preorder, inorder []string) (*BinaryTree, []string) {
+	node := &BinaryTree{preorder[0], nil, nil}
+	preorder = preorder[1:]
+	if len(inorder) == 1 {
+		return node, preorder
+	}
+	index := search(inorder, node.value)
+	node.left, preorder = Reconstruct(preorder, inorder[:index])
+	node.right, preorder = Reconstruct(preorder, inorder[index+1:])
+	return node, preorder
+}
+
+func search(strs []string, target string) int {
+	var result int
+	for index, str := range strs {
+		if str == target {
+			result = index
+			break
+		}
+	}
+	return result
+}
