5647b5b8d7065c41b9d680af7c101e2633db1aa5
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-11-03 21:38:59 -0600

    day 74: multiplication tables count. brute and linear solutions

---
+++ day74/problem_test.go
@@ -0,0 +1,44 @@
+package day74
+
+import "testing"
+
+var testcases = []struct {
+	N, X     int
+	expected int
+}{
+	{6, 12, 4},
+}
+
+func TestMultiplicationTableBrute(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := MultiplicationTableBrute(tc.N, tc.X); result != tc.expected {
+			t.Errorf("Given N=%d X=%d got %d expected %d", tc.N, tc.X, result, tc.expected)
+		}
+	}
+}
+
+func BenchmarkMultiplicationTableBrute(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MultiplicationTableBrute(tc.N, tc.X)
+		}
+	}
+}
+
+func TestMultiplicationTableLinear(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := MultiplicationTableLinear(tc.N, tc.X); result != tc.expected {
+			t.Errorf("Given N=%d X=%d got %d expected %d", tc.N, tc.X, result, tc.expected)
+		}
+	}
+}
+
+func BenchmarkMultiplicationTableLinear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MultiplicationTableLinear(tc.N, tc.X)
+		}
+	}
+}
