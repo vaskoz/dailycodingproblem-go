439ba1cde11a5ac582d89e0828212e9790f82e3b
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-11-14 16:26:24 -0700

    day 85: big perf boost from 10k ns/op to 522 ns/op

--- day85/problem.go
+++ day85/problem.go
@@ -3,9 +3,12 @@ package day85
 // MathematicalIf returns x if b=1 and y if b=0.
 func MathematicalIf(x, y, b int32) int32 {
 	var xMask, yMask int32
-	for i := uint32(0); i < 32; i++ {
-		xMask |= b << i
-	}
+	xMask = b
+	xMask |= xMask << 1
+	xMask |= xMask << 2
+	xMask |= xMask << 4
+	xMask |= xMask << 8
+	xMask |= xMask << 16
 	yMask = ^xMask
 	return (x & xMask) | (y & yMask)
 }
