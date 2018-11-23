4119fb83a465dd509ec8c1718ea0b892a1358855
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-11-22 21:50:53 -0600

    day 92: remove circular DAG testcase

--- day92/problem_test.go
+++ day92/problem_test.go
@@ -17,11 +17,6 @@ var testcases = []struct {
 		[]string{"CSC100", "CSC200", "CSC300"}},
 	{map[string][]string{
 		"CSC300": {"CSC100"},
-		"CSC100": {"CSC300"},
-	},
-		[]string{"CSC100", "CSC300"}},
-	{map[string][]string{
-		"CSC300": {"CSC100"},
 		"CSC400": {"CSC200"},
 	},
 		nil},
