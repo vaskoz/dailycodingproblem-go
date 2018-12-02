f3cd98110a0f439f3d0f51dd4a1239c06803c24a
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-02 15:47:33 -0700

    day 102: breakout non-negative and negative testcases.

--- day102/problem_test.go
+++ day102/problem_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var testcases = []struct {
+var nonNegativeTestcases = []struct {
 	input    []int
 	k        int
 	expected []int
@@ -18,9 +18,27 @@ var testcases = []struct {
 		49, []int{4, 5, 6, 7, 8, 9, 10}},
 }
 
+var negativeTestcases = []struct {
+	input    []int
+	k        int
+	expected []int
+}{
+	{[]int{-1, 2, 3, 4, 5}, 1, []int{-1, 2}},
+	{[]int{1, -2, 3, 4, 5}, 5, []int{-2, 3, 4}},
+	{[]int{1, 2, -3, 4, 5}, 6, []int{-3, 4, 5}},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+		15, 16, 17, 18, 19, 20, 21, 22, 23, -24, -25},
+		-49, []int{-24, -25}},
+}
+
 func TestContiguousSumBrute(t *testing.T) {
 	t.Parallel()
-	for _, tc := range testcases {
+	for _, tc := range nonNegativeTestcases {
+		if result := ContiguousSumBrute(tc.input, tc.k); !reflect.DeepEqual(tc.expected, result) {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+	for _, tc := range negativeTestcases {
 		if result := ContiguousSumBrute(tc.input, tc.k); !reflect.DeepEqual(tc.expected, result) {
 			t.Errorf("Expected %v got %v", tc.expected, result)
 		}
@@ -29,7 +47,10 @@ func TestContiguousSumBrute(t *testing.T) {
 
 func BenchmarkContiguousSumBrute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		for _, tc := range testcases {
+		for _, tc := range nonNegativeTestcases {
+			ContiguousSumBrute(tc.input, tc.k)
+		}
+		for _, tc := range negativeTestcases {
 			ContiguousSumBrute(tc.input, tc.k)
 		}
 	}
@@ -37,7 +58,7 @@ func BenchmarkContiguousSumBrute(b *testing.B) {
 
 func TestContiguousSumNonNegative(t *testing.T) {
 	t.Parallel()
-	for _, tc := range testcases {
+	for _, tc := range nonNegativeTestcases {
 		if result := ContiguousSumNonNegative(tc.input, tc.k); !reflect.DeepEqual(tc.expected, result) {
 			t.Errorf("Expected %v got %v", tc.expected, result)
 		}
@@ -56,7 +77,10 @@ func TestContiguousSumNonNegativePanic(t *testing.T) {
 
 func BenchmarkContiguousSumNonNegative(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		for _, tc := range testcases {
+		for _, tc := range nonNegativeTestcases {
+			ContiguousSumNonNegative(tc.input, tc.k)
+		}
+		for _, tc := range nonNegativeTestcases {
 			ContiguousSumNonNegative(tc.input, tc.k)
 		}
 	}
