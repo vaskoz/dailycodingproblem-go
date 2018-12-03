184d29a3b6bb123563705c72edf6885bb9bd26b0
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-03 16:25:41 -0700

    day 103: shortest substring containing all characters in set

---
+++ day103/problem_test.go
@@ -0,0 +1,31 @@
+package day103
+
+import "testing"
+
+var testcases = []struct {
+	str      string
+	letters  []rune
+	expected string
+}{
+	{"figehaeci", []rune("aei"), "aeci"},
+	{"figehaeci", []rune("xei"), ""},
+	{"this is a test string", []rune("tist"), "t stri"},
+	{"a", []rune("ab"), ""},
+}
+
+func TestShortestSubstringContains(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := ShortestSubstringContains(tc.str, tc.letters); result != tc.expected {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkShortestSubstringContains(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			ShortestSubstringContains(tc.str, tc.letters)
+		}
+	}
+}
