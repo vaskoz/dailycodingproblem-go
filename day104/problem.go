4e6540fe51318647cabed300ae355ff3f1cd10dc
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-04 16:23:47 -0700

    day 104: check if singly linked list is a palindrome by reversing half

--- day104/problem.go
+++ day104/problem.go
@@ -50,3 +50,48 @@ func IsPalindromeSinglyLLStack(head *SinglyLL) bool {
 	}
 	return head == nil
 }
+
+// IsPalindromeSinglyLLReverse answers the question "is this list a palindrome?".
+// Runs on a singly linked list.
+// Runs in O(N) time and O(1) space.
+// NOTE: This modifies the linked list during execution, but restores it after completion.
+func IsPalindromeSinglyLLReverse(head *SinglyLL) bool {
+	var n int
+	for tail := head; tail != nil; tail = tail.Next {
+		n++
+	}
+	halfPoint := head
+	for i := 0; i < (n/2)-1; i++ {
+		halfPoint = halfPoint.Next
+	}
+	if n%2 == 1 {
+		halfPoint = halfPoint.Next
+	}
+	reversed := ReverseSinglyLL(halfPoint.Next)
+	halfPoint.Next = reversed
+	isPalindrome := true
+	half := halfPoint.Next
+	for i := 0; i < n/2; i++ {
+		if half.Value != head.Value {
+			isPalindrome = false
+			break
+		}
+		half = half.Next
+		head = head.Next
+	}
+	halfPoint.Next = ReverseSinglyLL(halfPoint.Next)
+	return isPalindrome
+}
+
+// ReverseSinglyLL reverses a singly linked list in O(1) space
+// and O(N) time.
+func ReverseSinglyLL(head *SinglyLL) *SinglyLL {
+	var reversed *SinglyLL
+	for head != nil {
+		next := head.Next
+		head.Next = reversed
+		reversed = head
+		head = next
+	}
+	return reversed
+}
--- day104/problem_test.go
+++ day104/problem_test.go
@@ -47,3 +47,50 @@ func BenchmarkIsPalindromeSinglyLLStack(b *testing.B) {
 		}
 	}
 }
+
+func TestIsPalindromeSinglyLLReverse(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		head := createSinglyLL(tc.data)
+		if result := IsPalindromeSinglyLLReverse(head); result != tc.isPalindrome {
+			t.Errorf("For input %v expected %v got %v", tc.data, tc.isPalindrome, result)
+		}
+	}
+}
+
+func BenchmarkIsPalindromeSinglyLLReverse(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			head := createSinglyLL(tc.data)
+			IsPalindromeSinglyLLReverse(head)
+		}
+	}
+}
+
+func createDoublyLL(input []int) *DoublyLL {
+	if len(input) == 0 {
+		return nil
+	}
+	head := &DoublyLL{input[0], nil, nil}
+	current := head
+	for i := 1; i < len(input); i++ {
+		next := &DoublyLL{Value: input[i], Prev: current, Next: nil}
+		current.Next = next
+		current = next
+	}
+	return head
+}
+
+func createSinglyLL(input []int) *SinglyLL {
+	if len(input) == 0 {
+		return nil
+	}
+	head := &SinglyLL{input[0], nil}
+	current := head
+	for i := 1; i < len(input); i++ {
+		next := &SinglyLL{input[i], nil}
+		current.Next = next
+		current = next
+	}
+	return head
+}
