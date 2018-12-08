5499cda1550d121d20d28e79acb55ed45778e6d9
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-07 19:49:07 -0700

    day 107: print binary tree level-wise brute force

---
+++ day107/problem.go
@@ -0,0 +1,45 @@
+package day107
+
+// BinaryTree is a binary tree of integers.
+type BinaryTree struct {
+	Value       int
+	Left, Right *BinaryTree
+}
+
+// BinaryTreeByLevel prints the binary tree level-wise.
+func BinaryTreeByLevel(tree *BinaryTree) []int {
+	var result []int
+	for i := 0; i < Height(tree); i++ {
+		result = append(result, GetLevel(tree, i)...)
+	}
+	return result
+}
+
+// GetLevel returns all the nodes at the same height from left to right.
+func GetLevel(tree *BinaryTree, level int) []int {
+	if tree == nil {
+		return nil
+	}
+	if level == 0 {
+		return []int{tree.Value}
+	}
+	var result []int
+	result = append(result, GetLevel(tree.Left, level-1)...)
+	result = append(result, GetLevel(tree.Right, level-1)...)
+	return result
+}
+
+// Height returns the height of a binary tree.
+func Height(tree *BinaryTree) int {
+	if tree == nil {
+		return 0
+	}
+	return 1 + max(Height(tree.Left), Height(tree.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
