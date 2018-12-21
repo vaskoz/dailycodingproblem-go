6a304ffcccf7011f23c115ef1b7eb9c637f8b1d9
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-20 19:49:54 -0700

    day 118: linear time version of square elements sorted

--- day118/problem.go
+++ day118/problem.go
@@ -1,6 +1,8 @@
 package day118
 
-import "sort"
+import (
+	"sort"
+)
 
 // SquareElementsSortedBrute returns the squared result of every element of the param.
 // Runs in O(N log N) due to sorting the results.
@@ -10,5 +12,41 @@ func SquareElementsSortedBrute(sorted []int) []int {
 		result[i] = sorted[i] * sorted[i]
 	}
 	sort.Ints(result)
+	return result
+}
+
+// SquareElementsSortedLinear returns the squared result of every element of the param.
+// Runs in O(N) due to merging two sorted lists into the result.
+func SquareElementsSortedLinear(sorted []int) []int {
+	result := make([]int, 0, len(sorted))
+	firstPositiveIndex := 0
+	for i := range sorted {
+		if sorted[i] >= 0 {
+			firstPositiveIndex = i
+			break
+		}
+	}
+	negI, posI := firstPositiveIndex-1, firstPositiveIndex
+	for {
+		if negI >= 0 && posI < len(sorted) {
+			a := sorted[negI] * sorted[negI]
+			b := sorted[posI] * sorted[posI]
+			if a < b {
+				result = append(result, a)
+				negI--
+			} else {
+				result = append(result, b)
+				posI++
+			}
+		} else if negI >= 0 {
+			result = append(result, sorted[negI]*sorted[negI])
+			negI--
+		} else if posI < len(sorted) {
+			result = append(result, sorted[posI]*sorted[posI])
+			posI++
+		} else {
+			break
+		}
+	}
 	return result
 }
--- day118/problem_test.go
+++ day118/problem_test.go
@@ -9,6 +9,10 @@ var testcases = []struct {
 	input, expected []int
 }{
 	{[]int{-9, -2, 0, 2, 3}, []int{0, 4, 4, 9, 81}},
+	{[]int{-20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9,
+		-2, 0, 2, 3, 4, 5, 7, 10, 12, 14, 18, 20, 22, 24, 25, 26, 27, 29, 30},
+		[]int{0, 4, 4, 9, 16, 25, 49, 81, 100, 100, 121, 144, 144, 169, 196, 196,
+			225, 256, 289, 324, 324, 361, 400, 400, 484, 576, 625, 676, 729, 841, 900}},
 }
 
 func TestSquareElementsSortedBrute(t *testing.T) {
@@ -23,6 +27,22 @@ func BenchmarkSquareElementsSortedBrute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			SquareElementsSortedBrute(tc.input)
+		}
+	}
+}
+
+func TestSquareElementsSortedLinear(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := SquareElementsSortedLinear(tc.input); !reflect.DeepEqual(tc.expected, result) {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+func BenchmarkSquareElementsSortedLinear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			SquareElementsSortedLinear(tc.input)
 		}
 	}
 }
