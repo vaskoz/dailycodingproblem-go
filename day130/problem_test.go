281ac39bfd9bb22f84ff4e131640996bf7d4ae31
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-12-30 13:41:17 -0700

    day 130: stock profit with at most 'k' buy/sell transactions

---
+++ day130/problem_test.go
@@ -0,0 +1,28 @@
+package day130
+
+import "testing"
+
+var testcases = []struct {
+	prices    []int
+	k         int
+	maxProfit int
+}{
+	{[]int{5, 2, 4, 0, 1}, 2, 3},
+}
+
+func TestMaxProfit(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if maxProfit := MaxProfit(tc.prices, tc.k); maxProfit != tc.maxProfit {
+			t.Errorf("Expected %v got %v", tc.maxProfit, maxProfit)
+		}
+	}
+}
+
+func BenchmarkMaxProfit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MaxProfit(tc.prices, tc.k)
+		}
+	}
+}
