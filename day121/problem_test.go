679d7870ee00d0bf1956d5eb5225ab303c8f8327
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-01-09 06:03:35 -0700

    day 121: is k-palindrome dynamic programming

--- day121/problem_test.go
+++ day121/problem_test.go
@@ -26,3 +26,20 @@ func BenchmarkIsKPalindromeBrute(b *testing.B) {
 		}
 	}
 }
+
+func TestIsKPalindromeDP(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := IsKPalindromeDP(tc.input, tc.k); result != tc.expected {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkIsKPalindromeDP(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			IsKPalindromeDP(tc.input, tc.k)
+		}
+	}
+}
