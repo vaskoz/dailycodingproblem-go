5b171cf479b62022075ccc186012e3b7cd3d2331
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-01-08 19:40:07 -0700

    day 138: implement a smarter optimal change maker

--- day138/problem.go
+++ day138/problem.go
@@ -1,6 +1,8 @@
 package day138
 
-import "errors"
+import (
+	"errors"
+)
 
 var errNotPossible = errors.New("Can not make this value")
 
@@ -24,6 +26,32 @@ func MinCurrencyRequiredBrute(amt int, denomCents []int) (int, error) {
 		if count, err := MinCurrencyRequiredBrute(amt-denom, denomCents); err == nil && count+1 < min {
 			found = true
 			min = count + 1
+		}
+	}
+	if found {
+		return min, nil
+	}
+	return 0, errNotPossible
+}
+
+// MinCurrencyRequiredSmarter returns the minimum number of currency
+// necessary to make the desired amount.
+func MinCurrencyRequiredSmarter(amt int, denomCents []int) (int, error) {
+	return minCurrencyRequiredSmarter(amt, int(^uint(0)>>1), 0, denomCents)
+}
+
+func minCurrencyRequiredSmarter(amt, min, count int, denomCents []int) (int, error) {
+	if count > min || amt < 0 {
+		return 0, errNotPossible
+	} else if amt == 0 {
+		return count, nil
+	}
+	found := false
+	for i := range denomCents {
+		current := denomCents[len(denomCents)-1-i]
+		if result, err := minCurrencyRequiredSmarter(amt-current, min, count+1, denomCents); err == nil && result < min {
+			found = true
+			min = result
 		}
 	}
 	if found {
