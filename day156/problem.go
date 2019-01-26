9ff331f978d5ef23be315dfc38936babff67df59
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-01-25 18:26:47 -0700

    day 156: small performance improvement

--- day156/problem.go
+++ day156/problem.go
@@ -8,17 +8,20 @@ import (
 // of squared integers which sum to n.
 // Runs in exponential time.
 func MinSquared(n int) int {
+	return minSquared(n, 0, n)
+}
+
+func minSquared(n, level, minSoFar int) int {
 	if n <= 3 {
 		return n
 	}
-	result := n
 	end := int(math.Sqrt(float64(n))) + 1
 	for i := end; i > 0; i-- {
 		if x := i * i; x <= n {
-			if y := 1 + MinSquared(n-x); y < result {
-				result = y
+			if y := 1 + minSquared(n-x, level+1, minSoFar); y < minSoFar {
+				minSoFar = y
 			}
 		}
 	}
-	return result
+	return minSoFar
 }
