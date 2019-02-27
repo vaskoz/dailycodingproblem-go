f9c3451a98a9dde090c6d8b84e314a0a252b9df8
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-02-26 17:35:23 -0700

    day 188: simplify wrapped function

--- day188/problem.go
+++ day188/problem.go
@@ -15,12 +15,11 @@ func MakeFunctionsOriginal() []func() int {
 func MakeFunctionsCorrect() []func() int {
 	var funcList []func() int
 	for _, i := range []int{1, 2, 3} {
-		wrapped := func(i int) func() int {
+		funcList = append(funcList, func(i int) func() int {
 			return func() int {
 				return i
 			}
-		}
-		funcList = append(funcList, wrapped(i))
+		}(i))
 	}
 	return funcList
 }
