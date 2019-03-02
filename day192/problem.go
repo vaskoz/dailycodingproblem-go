592c660d1008647ea3d9f6641d697ffe8c72ad68
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-03-02 15:34:08 -0700

    day 192: linear version of reach the end problem

--- day192/problem.go
+++ day192/problem.go
@@ -12,3 +12,32 @@ func CanAdvanceToEndBrute(steps []int) bool {
 	}
 	return false
 }
+
+// CanAdvanceToEndLinear does it in one pass.
+func CanAdvanceToEndLinear(steps []int) bool {
+	if len(steps) < 2 {
+		return true
+	}
+	maxReach, step := steps[0], steps[0]
+	for i := 1; i < len(steps); i++ {
+		if i == len(steps)-1 {
+			return true
+		}
+		maxReach = max(maxReach, i+steps[i])
+		step--
+		if step == 0 {
+			if i >= maxReach {
+				break
+			}
+			step = maxReach - i
+		}
+	}
+	return false
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- day192/problem_test.go
+++ day192/problem_test.go
@@ -8,6 +8,8 @@ var testcases = []struct {
 }{
 	{[]int{1, 3, 1, 2, 0, 1}, true},
 	{[]int{1, 2, 1, 0, 0}, false},
+	{[]int{0}, true},
+	{[]int{}, true},
 }
 
 func TestCanAdvanceToEndBrute(t *testing.T) {
@@ -23,6 +25,23 @@ func BenchmarkCanAdvanceToEndBrute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			CanAdvanceToEndBrute(tc.advanceFrom)
+		}
+	}
+}
+
+func TestCanAdvanceToEndLinear(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if possible := CanAdvanceToEndLinear(tc.advanceFrom); possible != tc.possible {
+			t.Errorf("Given %v, expected %v, got %v", tc.advanceFrom, tc.possible, possible)
+		}
+	}
+}
+
+func BenchmarkCanAdvanceToEndLinear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			CanAdvanceToEndLinear(tc.advanceFrom)
 		}
 	}
 }
