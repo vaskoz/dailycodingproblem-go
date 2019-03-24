0ba066af39416aaa255106ee329c1178aa4b5fff
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-03-24 10:33:15 -0600

    day 214: convert to binary string and then count

--- day214/problem.go
+++ day214/problem.go
@@ -1,5 +1,7 @@
 package day214
 
+import "strconv"
+
 // LengthLongestConsecutiveRunOnesBitShift calculates the longest
 // run of 1's using bit shifting.
 func LengthLongestConsecutiveRunOnesBitShift(n int) int {
@@ -14,6 +16,29 @@ func LengthLongestConsecutiveRunOnesBitShift(n int) int {
 			count = 0
 		}
 		n >>= 1
+	}
+	if count > max {
+		return count
+	}
+	return max
+}
+
+// LengthLongestConsecutiveRunOnesString converts the number
+// into a binary representation string.
+func LengthLongestConsecutiveRunOnesString(n int) int {
+	var max, count int
+	for _, digit := range strconv.FormatInt(int64(n), 2) {
+		if digit == '1' {
+			count++
+		} else {
+			if count > max {
+				max = count
+			}
+			count = 0
+		}
+	}
+	if count > max {
+		return count
 	}
 	return max
 }
--- day214/problem_test.go
+++ day214/problem_test.go
@@ -7,6 +7,8 @@ var testcases = []struct {
 	longestConsecutiveOnes int
 }{
 	{156, 3},
+	{20432348234, 3},
+	{65535, 16},
 }
 
 func TestLengthLongestConsecutiveRunOnesBitShift(t *testing.T) {
@@ -22,6 +24,23 @@ func BenchmarkLengthLongestConsecutiveRunOnesBitShift(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			LengthLongestConsecutiveRunOnesBitShift(tc.n)
+		}
+	}
+}
+
+func TestLengthLongestConsecutiveRunOnesString(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := LengthLongestConsecutiveRunOnesString(tc.n); result != tc.longestConsecutiveOnes {
+			t.Errorf("Given n=%d, expected %d, got %d", tc.n, tc.longestConsecutiveOnes, result)
+		}
+	}
+}
+
+func BenchmarkLengthLongestConsecutiveRunOnesString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			LengthLongestConsecutiveRunOnesString(tc.n)
 		}
 	}
 }
