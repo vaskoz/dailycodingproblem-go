d2044b24c4a2bc0749f0ecd66c2e911fbcfaee36
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-04 18:03:55 -0600

    day 225: remove unnecessary function call for k=2 version

--- day225/problem.go
+++ day225/problem.go
@@ -4,16 +4,16 @@ import "math/bits"
 
 // JosephusGeneral is the general case solution.
 func JosephusGeneral(n, k int) int {
+	if n < 1 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
 	if k == 2 {
-		return josephusGeneralK2(n)
+		highestBitValue := 1 << uint(bits.Len64(uint64(n))-1)
+		return 2*(n-highestBitValue) + 1
 	}
 	return josephusGeneral(n, k) + 1
-}
-
-func josephusGeneralK2(n int) int {
-	highestBit := bits.Len64(uint64(n))
-	highestBit = 1 << uint(highestBit-1)
-	return 2*(n-highestBit) + 1
 }
 
 func josephusGeneral(n, k int) int {
