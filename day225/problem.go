6bfd7a202712da460bb5bd53c8f26efec49e0727
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-04 18:14:50 -0600

    day 225: replace if-else with switch

--- day225/problem.go
+++ day225/problem.go
@@ -4,16 +4,17 @@ import "math/bits"
 
 // JosephusGeneral is the general case solution.
 func JosephusGeneral(n, k int) int {
-	if n < 1 {
+	switch {
+	case n < 1:
 		return 0
-	} else if n == 1 {
+	case n == 1:
 		return 1
-	}
-	if k == 2 {
+	case k == 2:
 		highestBitValue := 1 << uint(bits.Len64(uint64(n))-1)
 		return 2*(n-highestBitValue) + 1
+	default:
+		return josephusGeneral(n, k) + 1
 	}
-	return josephusGeneral(n, k) + 1
 }
 
 func josephusGeneral(n, k int) int {
