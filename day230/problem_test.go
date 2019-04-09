6e026991a1a0fa90708611bb565c16f4fc809f33
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-09 16:10:22 -0600

    day 230: dynamic programming solution to egg drop

--- day230/problem_test.go
+++ day230/problem_test.go
@@ -9,6 +9,33 @@ var testcases = []struct {
 	{2, 10, 4},
 }
 
+func TestEggDropDP(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := EggDropDP(tc.eggs, tc.floors); result != tc.minimum {
+			t.Errorf("Expected %d, got %d", tc.minimum, result)
+		}
+	}
+}
+
+func TestEggDropDPPanic(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("expected a panic for negative values")
+		}
+	}()
+	EggDropDP(-1, 0)
+}
+
+func BenchmarkEggDropDP(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			EggDropDP(tc.eggs, tc.floors)
+		}
+	}
+}
+
 func TestEggDropRecursive(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testcases {
