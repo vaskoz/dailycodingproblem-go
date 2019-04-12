ded312742ab17372f46109f376d6921e2c635245
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-12 10:15:36 -0600

    day 233: add overflow detection

--- day233/problem.go
+++ day233/problem.go
@@ -1,13 +1,27 @@
 package day233
 
+import "errors"
+
+var errOverflow = errors.New("overflow occurred")
+
+// ErrOverflow represents an overflow error.
+func ErrOverflow() error {
+	return errOverflow
+}
+
 // FastFibonnaci runs in O(N) time with O(1) space.
-func FastFibonnaci(n uint) uint {
+// Returns an error if overflow occurs.
+func FastFibonnaci(n uint) (uint, error) {
 	if n < 2 {
-		return n
+		return n, nil
 	}
 	first, second := uint(0), uint(1)
+	var err error
 	for i := uint(1); i < n; i++ {
 		first, second = second, first+second
+		if second < first {
+			return 0, errOverflow
+		}
 	}
-	return second
+	return second, err
 }
--- day233/problem_test.go
+++ day233/problem_test.go
@@ -4,21 +4,23 @@ import "testing"
 
 var testcases = []struct {
 	n, fib uint
+	err    error
 }{
-	{0, 0},
-	{1, 1},
-	{2, 1},
-	{12, 144},
-	{28, 317811},
-	{44, 701408733},
-	{87, 679891637638612258},
+	{0, 0, nil},
+	{1, 1, nil},
+	{2, 1, nil},
+	{12, 144, nil},
+	{28, 317811, nil},
+	{44, 701408733, nil},
+	{87, 679891637638612258, nil},
+	{100, 0, ErrOverflow()},
 }
 
 func TestFastFibonnaci(t *testing.T) {
 	t.Parallel()
 	for _, tc := range testcases {
-		if result := FastFibonnaci(tc.n); result != tc.fib {
-			t.Errorf("Given n=%d, expected %d, got %d", tc.n, tc.fib, result)
+		if result, err := FastFibonnaci(tc.n); result != tc.fib || err != tc.err {
+			t.Errorf("Given n=%d, expected (%d,%v), got (%d,%v)", tc.n, tc.fib, tc.err, result, err)
 		}
 	}
 }
@@ -26,7 +28,7 @@ func TestFastFibonnaci(t *testing.T) {
 func BenchmarkFastFibonnaci(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
-			FastFibonnaci(tc.n)
+			FastFibonnaci(tc.n) //nolint
 		}
 	}
 }
