1a023cf85f7fee74e24ab387db1ea3039f2c8fec
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-14 11:26:50 -0600

    day 235: faster by pairs

--- day235/problem.go
+++ day235/problem.go
@@ -19,3 +19,33 @@ func MaxMinNotGoodEnough(nums []int) (min, max int) {
 	}
 	return min, max
 }
+
+// MaxMinPairs returns the minimum and maximum values
+// from the slice of integers.
+// Runs in 3N/2 which is smaller than 2*(N-2)
+func MaxMinPairs(nums []int) (min, max int) {
+	if len(nums) == 0 {
+		return 0, 0
+	} else if len(nums) == 1 {
+		return nums[0], nums[0]
+	}
+	min, max = nums[0], nums[1]
+	start := 2
+	if len(nums)%2 == 1 {
+		min, max = nums[0], nums[0]
+		start = 1
+	}
+	for i := start; i < len(nums); i += 2 {
+		ma, mi := nums[i], nums[i+1]
+		if mi > ma {
+			ma, mi = mi, ma
+		}
+		if ma > max {
+			max = ma
+		}
+		if mi < min {
+			min = mi
+		}
+	}
+	return min, max
+}
--- day235/problem_test.go
+++ day235/problem_test.go
@@ -8,6 +8,7 @@ var testcases = []struct {
 }{
 	{[]int{3, 3, 2, 3, 5, 6, 3, 2, 3, 3, 3, 2, -10, 323, 3, 3, 3, 35, 5, 6, 6, 6, 500, 23, 320, 320, 10}, -10, 500},
 	{nil, 0, 0},
+	{[]int{4}, 4, 4},
 }
 
 func TestMaxMinNotGoodEnough(t *testing.T) {
@@ -23,6 +24,23 @@ func BenchmarkMaxMinNotGoodEnough(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testcases {
 			MaxMinNotGoodEnough(tc.nums)
+		}
+	}
+}
+
+func TestMaxMinPairs(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if min, max := MaxMinPairs(tc.nums); min != tc.min || max != tc.max {
+			t.Errorf("Expected (%d,%d) got (%d,%d)", tc.min, tc.max, min, max)
+		}
+	}
+}
+
+func BenchmarkMaxMinPairs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MaxMinPairs(tc.nums)
 		}
 	}
 }
