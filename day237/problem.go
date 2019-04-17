33a7f7ef82dafbd32e650b5aeafc1e666584dcf5
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-16 19:04:42 -0600

    day 237: compact and recursive

--- day237/problem.go
+++ day237/problem.go
@@ -13,43 +13,18 @@ type KaryTree struct {
 // IsTreeSymmetric answers if its data and shape remain
 // unchanged when it is reflected about the root node.
 func IsTreeSymmetric(tree *KaryTree) bool {
-	if tree == nil {
-		return true
-	}
-	level := tree.Children
-	for {
-		nextLevel := make([]*KaryTree, 0, len(level)*2)
-		for _, l := range level {
-			if l == nil {
-				nextLevel = append(nextLevel, l)
-			} else {
-				nextLevel = append(nextLevel, l.Children...)
-			}
-		}
-		for i := 0; i < len(nextLevel)/2; i++ {
-			if !equal(nextLevel[i], nextLevel[len(nextLevel)-1-i]) {
-				return false
-			}
-		}
-		done := true
-		for i := range nextLevel {
-			if nextLevel[i] != nil {
-				done = false
-				break
-			}
-		}
-		if done {
-			break
-		}
-		level = nextLevel
-	}
-	return true
+	return equal(tree, tree)
 }
 
 func equal(a, b *KaryTree) bool {
 	if a == nil && b == nil {
 		return true
 	} else if a != nil && b != nil && a.Value == b.Value {
+		for i := range a.Children {
+			if !equal(a.Children[i], b.Children[len(a.Children)-1-i]) {
+				return false
+			}
+		}
 		return true
 	}
 	return false
