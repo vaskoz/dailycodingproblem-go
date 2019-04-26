9a1704a7d17afbab0c02ff5e3010dd5e7690ebe3
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-26 15:12:13 -0600

    day 247: height balanced tree checker

---
+++ day247/problem.go
@@ -0,0 +1,43 @@
+package day247
+
+// BinaryTree is a simple binary tree without a value.
+type BinaryTree struct {
+	// Value int // is unnecessary. We don't need any value.
+	Left, Right *BinaryTree
+}
+
+// IsHeightBalanced returns true if the tree is height balanced.
+// It returns false otherwise.
+func IsHeightBalanced(root *BinaryTree) bool {
+	_, balanced := isHeightBalanced(root)
+	return balanced
+}
+
+func isHeightBalanced(n *BinaryTree) (int, bool) {
+	if n == nil {
+		return 0, true
+	}
+	leftHeight, leftBalance := isHeightBalanced(n.Left)
+	rightHeight, rightBalance := isHeightBalanced(n.Right)
+	if !leftBalance || !rightBalance {
+		return 0, false
+	}
+	if diff := abs(leftHeight - rightHeight); diff > 1 {
+		return 0, false
+	}
+	return max(leftHeight, rightHeight) + 1, true
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
---
+++ day247/problem_test.go
@@ -0,0 +1,61 @@
+package day247
+
+import "testing"
+
+// nolint
+var testcases = []struct {
+	root           *BinaryTree
+	heightBalanced bool
+}{
+	{
+		&BinaryTree{
+			&BinaryTree{nil, nil},
+			&BinaryTree{nil, nil},
+		},
+		true,
+	},
+	{
+		&BinaryTree{
+			nil,
+			&BinaryTree{nil, nil},
+		},
+		true,
+	},
+	{
+		&BinaryTree{
+			nil,
+			&BinaryTree{&BinaryTree{nil, nil}, nil},
+		},
+		false,
+	},
+	{
+		&BinaryTree{
+			nil,
+			&BinaryTree{
+				&BinaryTree{
+					nil,
+					&BinaryTree{nil, nil},
+				},
+				nil,
+			},
+		},
+		false,
+	},
+}
+
+func TestIsHeightBalanced(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if result := IsHeightBalanced(tc.root); result != tc.heightBalanced {
+			t.Errorf("expected %v, got %v", tc.heightBalanced, result)
+		}
+	}
+}
+
+func BenchmarkIsHeightBalanced(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			IsHeightBalanced(tc.root)
+		}
+	}
+}
