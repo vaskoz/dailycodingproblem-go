6c7f7f9f24595c2b5be8043014823d7f21bb5957
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-06-03 11:11:08 -0600

    day 285: add runtime complexity comment

--- day285/problem.go
+++ day285/problem.go
@@ -3,6 +3,7 @@ package day285
 // SunsetViews returns the number of building with some amount
 // of sunset views.
 // Buildings are ordered from east to west.
+// Runs in O(N) time and O(1) space.
 func SunsetViews(buildingHeights []int) int {
 	if len(buildingHeights) == 0 {
 		return 0
