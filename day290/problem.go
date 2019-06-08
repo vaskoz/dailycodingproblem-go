1459d8dc030d3fffac016f1401839a6d0300b5f4
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-06-08 16:50:20 -0600

    day 290: more efficient version

--- day290/problem.go
+++ day290/problem.go
@@ -1,26 +1,23 @@
 package day290
 
-import "reflect"
-
 // MinimizeQux returns the smallest Qux slice
 // possible.
 // Runs in O(N*K) where K is the number of elements that can removed.
-// Uses O(2*N) extra space to avoid modifying the input.
+// Uses O(N) extra space to avoid modifying the input.
 func MinimizeQux(input []rune) []rune {
-	prev := make([]rune, len(input))
-	next := make([]rune, len(input))
-	copy(next, input)
-	for !reflect.DeepEqual(prev, next) {
-		prev = make([]rune, len(next))
-		copy(prev, next)
-		for i := 1; i < len(next); i++ {
-			if next[i] != next[i-1] {
-				missing := 'R' + 'G' + 'B' - next[i] - next[i-1]
+	copied := make([]rune, len(input))
+	copy(copied, input)
+	var lastLen int
+	for len(copied) != lastLen {
+		lastLen = len(copied)
+		for i := 1; i < lastLen; i++ {
+			if copied[i] != copied[i-1] {
+				missing := 'R' + 'G' + 'B' - copied[i] - copied[i-1]
 				tmp := []rune{missing}
-				next = append(next[:i-1], append(tmp, next[i+1:]...)...)
+				copied = append(copied[:i-1], append(tmp, copied[i+1:]...)...)
 				break
 			}
 		}
 	}
-	return next
+	return copied
 }
