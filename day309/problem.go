dde9c6b25a464d1ea1bc6c9974236d7eae55bc81
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-06-27 22:25:41 -0600

    day 309: add a faster version

--- day309/problem.go
+++ day309/problem.go
@@ -1,10 +1,66 @@
 package day309
 
+// MinRedistributeNoGapsFaster returns the minimum cost
+// to move all together.
+// Runs in O(N) time and O(1) space.
+func MinRedistributeNoGapsFaster(seating []int) int {
+	var min int
+	totalPeople := 0
+	for i := range seating {
+		if seating[i] == 1 {
+			totalPeople++
+		}
+	}
+	if totalPeople < 2 {
+		return 0
+	}
+	middle := totalPeople / 2
+	if totalPeople%2 == 1 {
+		middle++
+	}
+	index := 0
+	for i := range seating {
+		if seating[i] == 1 {
+			middle--
+		}
+		if middle == 0 {
+			index = i
+			break
+		}
+	}
+	// left
+	leftZero := 0
+	leftZeroFound := false
+	for j := index - 1; j >= 0; j-- {
+		if !leftZeroFound && seating[j] == 0 {
+			leftZero = j
+			leftZeroFound = true
+		} else if seating[j] == 1 && j < leftZero {
+			min += abs(j - leftZero)
+			leftZero--
+		}
+	}
+	// right
+	rightZero := 0
+	rightZeroFound := false
+	for j := index + 1; j < len(seating); j++ {
+		if !rightZeroFound && seating[j] == 0 {
+			rightZero = j
+			rightZeroFound = true
+		} else if seating[j] == 1 && j > rightZero {
+			min += abs(j - rightZero)
+			rightZero++
+		}
+	}
+	return min
+}
+
 // MinRedistributeNoGapsBrute returns the minimum cost
 // to move all together.
 // Runs in O(N^2) time and O(1) space.
 func MinRedistributeNoGapsBrute(seating []int) int {
-	min := int(^uint(0) >> 1)
+	const MaxInt = int(^uint(0) >> 1)
+	min := MaxInt
 	for i := range seating {
 		var curr int
 		if seating[i] == 1 {
@@ -29,13 +85,16 @@ func MinRedistributeNoGapsBrute(seating []int) int {
 					rightZeroFound = true
 				} else if seating[j] == 1 && j > rightZero {
 					curr += abs(j - rightZero)
-					rightZero--
+					rightZero++
 				}
 			}
 			if curr < min {
 				min = curr
 			}
 		}
+	}
+	if min == MaxInt {
+		return 0
 	}
 	return min
 }
--- day309/problem_test.go
+++ day309/problem_test.go
@@ -8,6 +8,26 @@ var testcases = []struct {
 	minMoves int
 }{
 	{[]int{0, 1, 1, 0, 1, 0, 0, 0, 1}, 5},
+	{[]int{0, 0, 1, 0, 0, 0, 0, 0, 0}, 0},
+	{[]int{0, 0, 0, 0, 0, 0, 0, 0, 0}, 0},
+	{[]int{1, 0, 1, 0, 1, 0, 1, 0, 1}, 6},
+}
+
+func TestMinRedistributeNoGapsFaster(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if minMoves := MinRedistributeNoGapsFaster(tc.seating); minMoves != tc.minMoves {
+			t.Errorf("Expected %v, got %v", tc.minMoves, minMoves)
+		}
+	}
+}
+
+func BenchmarkMinRedistributeNoGapsFaster(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			MinRedistributeNoGapsFaster(tc.seating)
+		}
+	}
 }
 
 func TestMinRedistributeNoGapsBrute(t *testing.T) {
