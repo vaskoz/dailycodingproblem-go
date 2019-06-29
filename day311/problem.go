57cfd6fc3a5457756b8b834d18ac481fe05e243b
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-06-29 12:29:41 -0600

    day 311: add faster O(log N) solution

--- day311/problem.go
+++ day311/problem.go
@@ -1,5 +1,27 @@
 package day311
 
+// PeakIndex returns the index of a peak.
+// Runs in O(log N).
+func PeakIndex(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	} else if len(nums) == 1 {
+		return 0
+	}
+	return peakIndex(nums, 0, len(nums)-1)
+}
+
+func peakIndex(nums []int, low, high int) int {
+	mid := (low + high) / 2
+	if (mid == 0 || nums[mid-1] <= nums[mid]) &&
+		(mid == len(nums)-1 || nums[mid+1] <= nums[mid]) {
+		return mid
+	} else if mid > 0 && nums[mid-1] > nums[mid] {
+		return peakIndex(nums, low, mid-1)
+	}
+	return peakIndex(nums, mid+1, high)
+}
+
 // PeakIndexBrute returns the index of a peak.
 // Runs in O(N).
 func PeakIndexBrute(nums []int) int {
--- day311/problem_test.go
+++ day311/problem_test.go
@@ -10,8 +10,25 @@ var testcases = []struct {
 	{[]int{1, 3, 20, 4, 1, 0}, 2},
 	{nil, -1},
 	{[]int{100}, 0},
-	{[]int{5, 3, 20, 4, 1, 0}, 0},
+	{[]int{5, 3, 2, 1, 0}, 0},
 	{[]int{1, 3, 5, 8, 10, 100}, 5},
+}
+
+func TestPeakIndex(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if peak := PeakIndex(tc.nums); peak != tc.peakIndex {
+			t.Errorf("Expected %v, got %v", tc.peakIndex, peak)
+		}
+	}
+}
+
+func BenchmarkPeakIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			PeakIndex(tc.nums)
+		}
+	}
 }
 
 func TestPeakIndexBrute(t *testing.T) {
