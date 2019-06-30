014393b6e73331a1db88a8d48da98ae783a2dd05
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-06-30 15:04:57 -0600

    day 312: iterative fib for tiling problem

--- day312/problem.go
+++ day312/problem.go
@@ -1,11 +1,13 @@
 package day312
 
+const panicMessage = "n must be greater than 0"
+
 // TilingBoardWays returns the number of ways a board of size
 // 2 x N can be tiled using dominoes and trominoes.
 func TilingBoardWays(n int) int {
 	switch {
 	case n < 1:
-		panic("n must be greater than 0")
+		panic(panicMessage)
 	case n == 1:
 		return 1
 	case n == 2:
@@ -13,4 +15,19 @@ func TilingBoardWays(n int) int {
 	default:
 		return TilingBoardWays(n-1) + TilingBoardWays(n-2)
 	}
+}
+
+// TilingBoardWaysFaster returns the number of ways a board of size
+// 2 x N can be tiled using dominoes and trominoes.
+func TilingBoardWaysFaster(n int) int {
+	if n < 1 {
+		panic("n must be greater than 0")
+	}
+	n1, n2 := 0, 1
+	for i := 1; i <= n; i++ {
+		tmp := n2
+		n2 = n1 + n2
+		n1 = tmp
+	}
+	return n2
 }
--- day312/problem_test.go
+++ day312/problem_test.go
@@ -11,6 +11,34 @@ var testcases = []struct {
 	{2, 2},
 	{3, 3},
 	{4, 5},
+	{5, 8},
+}
+
+func TestTilingBoardWaysFaster(t *testing.T) {
+	t.Parallel()
+	for _, tc := range testcases {
+		if ways := TilingBoardWaysFaster(tc.n); ways != tc.tilingWays {
+			t.Errorf("Expected %v, got %v", tc.tilingWays, ways)
+		}
+	}
+}
+
+func TestTilingBoardWaysFasterBadInput(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("Expected a panic for a value less than 1")
+		}
+	}()
+	TilingBoardWaysFaster(0)
+}
+
+func BenchmarkTilingBoardWaysFaster(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			TilingBoardWaysFaster(tc.n)
+		}
+	}
 }
 
 func TestTilingBoardWays(t *testing.T) {
