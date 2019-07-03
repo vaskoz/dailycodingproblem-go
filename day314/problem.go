8afe701cea1119e3f5b7c4e6ae1b821c89488ae9
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-07-02 18:57:40 -0600

    day 314: radio tower necessary range in O(N) time

---
+++ day314/problem.go
@@ -0,0 +1,40 @@
+package day314
+
+import "sort"
+
+// MinimumTowerRange returns the minimum range of a tower to
+// cover all listeners.
+// Assumes listeners and towers are sorted in ascending order.
+// NOTE: This sorts these slices, if they're not already.
+// Runs in O(N) time and O(1) space.
+func MinimumTowerRange(listeners, towers []int) int {
+	if !sort.IntsAreSorted(listeners) {
+		sort.Ints(listeners)
+	}
+	if !sort.IntsAreSorted(towers) {
+		sort.Ints(towers)
+	}
+	var minRange int
+	towerIndex := 0
+	for _, listener := range listeners {
+		dist := abs(listener - towers[towerIndex])
+		for i := towerIndex; i < len(towers); i++ {
+			if d := abs(listener - towers[i]); d > dist {
+				break
+			} else {
+				dist = d
+			}
+		}
+		if dist > minRange {
+			minRange = dist
+		}
+	}
+	return minRange
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
