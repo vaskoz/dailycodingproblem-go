d2aa67a71375f1e52ed3803a7761e9aa5d09376c
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-07-09 23:15:33 -0500

    day 320: smallest window with all distinct characters

---
+++ day320/problem.go
@@ -0,0 +1,35 @@
+package day320
+
+// SmallestWindowEveryDistinctLength returns the length
+// of the smallest window that contains every distinct
+// character available.
+// Runs in O(N) time and O(k) extra space where k
+// is the number of distinct characters.
+func SmallestWindowEveryDistinctLength(str string) int {
+	distinct := make(map[rune]int)
+	for _, r := range str {
+		distinct[r]++
+	}
+	distinctCount := len(distinct)
+	distinct = make(map[rune]int, distinctCount)
+	var begin int
+	smallest := len(str)
+	for end, r := range str {
+		distinct[r]++
+		if len(distinct) == distinctCount {
+			length := end - begin + 1
+			if length < smallest {
+				smallest = length
+			}
+		}
+		for len(distinct) == distinctCount {
+			toRemove := rune(str[begin])
+			distinct[toRemove]--
+			if count := distinct[toRemove]; count == 0 {
+				delete(distinct, toRemove)
+			}
+			begin++
+		}
+	}
+	return smallest
+}
