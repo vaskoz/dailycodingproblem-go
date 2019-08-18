054dc80872bff0414173936f300379e2ca4557b1
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-08-17 18:54:47 -0600

    day 360: use github.com/algds/dfstopo as a dependency

--- day360/problem.go
+++ day360/problem.go
@@ -2,6 +2,8 @@ package day360
 
 import "errors"
 
+import "github.com/algds/dfstopo"
+
 // Song represents a particular song.
 type Song int
 
@@ -20,16 +22,16 @@ var errPlaylistNotPossible = errors.New("not possible to produce a playlist with
 // to return a satisfactory playlist or an error if
 // not possible.
 func InterleaveSongsByRank(ranks []Playlist) ([]Song, error) {
-	g := make(directedGraph)
+	dg := make(dfstopo.DirectedGraph)
 	for _, rank := range ranks {
 		for i := 1; i < len(rank); i++ {
-			if g[rank[i-1]] == nil {
-				g[rank[i-1]] = make(map[node]struct{})
+			if dg[rank[i-1]] == nil {
+				dg[rank[i-1]] = make(map[dfstopo.Node]struct{})
 			}
-			g[rank[i-1]][rank[i]] = struct{}{}
+			dg[rank[i-1]][rank[i]] = struct{}{}
 		}
 	}
-	nodes, err := dfsTopo(g)
+	nodes, err := dfstopo.Sort(dg)
 	if err != nil {
 		return nil, errPlaylistNotPossible
 	}
@@ -38,60 +40,4 @@ func InterleaveSongsByRank(ranks []Playlist) ([]Song, error) {
 		songs[i] = nodes[i].(Song)
 	}
 	return songs, nil
-}
-
-type node interface{}
-
-type directedGraph map[node]map[node]struct{}
-
-func dfsTopo(g directedGraph) ([]node, error) {
-	var result []node
-	marks := make(map[node]int)
-	for from, to := range g {
-		marks[from] = 0
-		for t := range to {
-			marks[t] = 0
-		}
-	}
-	for n, mark := range marks {
-		if mark == 0 { // unmarked
-			tmp := make([]node, 0, len(marks))
-			nodes, err := visit(n, g, marks)
-			if err == nil {
-				tmp = append(tmp, nodes...)
-				tmp = append(tmp, result...)
-				result = tmp
-			} else {
-				return nil, err
-			}
-		}
-	}
-	return result, nil
-}
-
-func visit(n node, g directedGraph, marks map[node]int) ([]node, error) {
-	if marks[n] == 2 {
-		return nil, nil
-	} else if marks[n] == 1 {
-		return nil, errPlaylistNotPossible
-	}
-	marks[n] = 1
-	result := make([]node, 0, len(marks))
-	for m := range g[n] {
-		tmp := make([]node, 0, len(marks))
-		nodes, err := visit(m, g, marks)
-		if err == nil {
-			tmp = append(tmp, nodes...)
-			tmp = append(tmp, result...)
-			result = tmp
-		} else {
-			return nil, err
-		}
-	}
-	marks[n] = 2
-	tmp := make([]node, 0, len(marks))
-	tmp = append(tmp, n)
-	tmp = append(tmp, result...)
-	result = tmp
-	return result, nil
 }
