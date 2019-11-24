c3ee50999194305ca2cf3fa47acc6ff103e70874
Gitea <vaskoz@users.noreply.github.com>
2019-11-24 04:54:00 -0700

    day 68: add testcase suggested by @dhanendraverma

--- day68/problem_test.go
+++ day68/problem_test.go
@@ -2,15 +2,18 @@ package day68
 
 import "testing"
 
+// nolint
 var testcases = []struct {
 	bishops  []BoardPosition
 	expected int
 }{
 	{[]BoardPosition{{0, 0}, {1, 2}, {2, 2}, {4, 0}}, 2},
+	{[]BoardPosition{{0, 0}, {1, 4}, {2, 2}, {0, 1}, {4, 0}, {2, 3}, {4, 4}}, 6},
 }
 
 func TestCountAttackingBishopPairs(t *testing.T) {
 	t.Parallel()
+
 	for _, tc := range testcases {
 		if result := CountAttackingBishopPairs(tc.bishops); result != tc.expected {
 			t.Errorf("Expected %v got %v", tc.expected, result)
