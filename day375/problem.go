f6e6a2096a42b886c027fd3922d278e48440b987
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-11-29 12:01:01 -0700

    day 375: fewer LoC but slower
    Interesting that using sort.Reverse was slower than
    defining my own reverse using sort.Slice

---
+++ day375/problem.go
@@ -0,0 +1,18 @@
+package day375
+
+import "sort"
+
+// HIndex returns the h-index for this author.
+func HIndex(citations []int) int {
+	copied := append([]int{}, citations...)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(copied)))
+
+	for index, citation := range copied {
+		if citation < index+1 {
+			return index
+		}
+	}
+
+	return len(copied)
+}
---
+++ day375/problem_test.go
@@ -0,0 +1,31 @@
+package day375
+
+import "testing"
+
+// nolint
+var testcases = []struct {
+	citations []int
+	hindex    int
+}{
+	{[]int{4, 3, 0, 1, 5}, 3},
+	{[]int{10}, 1},
+	{[]int{4, 0, 0, 2, 3}, 2},
+}
+
+func TestHIndex(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range testcases {
+		if result := HIndex(tc.citations); result != tc.hindex {
+			t.Errorf("Expected %v, got %v", tc.hindex, result)
+		}
+	}
+}
+
+func BenchmarkHIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			HIndex(tc.citations)
+		}
+	}
+}
