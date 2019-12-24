e483786dd1ec0837dac4d03249cb91734da91e4c
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-12-24 15:25:58 -0700

    day 140: just a repeat of day 149

---
+++ day400/problem.go
@@ -0,0 +1,13 @@
+package day149
+
+// SumSublist returns the sum of the sublist from start (inclusive)
+// up to end (exclusive).
+// Runs in O(end-start).
+func SumSublist(l []int, start, end int) int {
+	var sum int
+	for i := start; i < end; i++ {
+		sum += l[i]
+	}
+
+	return sum
+}
