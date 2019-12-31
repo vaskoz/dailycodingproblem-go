55241f534b9e2a7eb6a8cc90f44ece063e5a4034
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-12-31 15:16:25 -0700

    day 407: identical to day 299 and a reminder
    of minimum spanning trees and kruskals alg

---
+++ day407/problem.go
@@ -0,0 +1,8 @@
+package day407
+
+import "github.com/algds/kruskals"
+
+// LowestCostPipeConfiguration is a MST for the given pipes.
+func LowestCostPipeConfiguration(pipes []kruskals.WeightedEdge) []kruskals.WeightedEdge {
+	return kruskals.MinimumSpanningTree(pipes)
+}
