8fc8370f4c483e71e5e2a0a3bc53eb65c5b4a4ea
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2020-02-02 03:37:22 -0700

    day 438: cleaned up version of day 154

---
+++ day438/problem_test.go
@@ -0,0 +1,45 @@
+package day438
+
+import "testing"
+
+// nolint
+var testcases = []struct {
+	items []interface{}
+}{
+	{[]interface{}{"foo", "bar", 1, 3, 5}},
+	{[]interface{}{10, 20, 30, 15, 30, 55}},
+}
+
+func TestStackHeap(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range testcases {
+		var stack StackHeap
+
+		for i := range tc.items {
+			stack.Push(tc.items[i])
+		}
+
+		for i := range tc.items {
+			if result := stack.Pop(); result != tc.items[len(tc.items)-1-i] {
+				t.Errorf("Expected %v got %v", tc.items[len(tc.items)-1-i], result)
+			}
+		}
+	}
+}
+
+func BenchmarkStackHeap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			var stack StackHeap
+
+			for i := range tc.items {
+				stack.Push(tc.items[i])
+			}
+
+			for range tc.items {
+				stack.Pop()
+			}
+		}
+	}
+}
