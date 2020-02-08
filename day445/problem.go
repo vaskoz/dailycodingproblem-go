81ab9fee3e60b9d7026bb2f7535fc1335fb127dd
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2020-02-08 11:24:24 -0700

    day 445: cleaned up day 146

---
+++ day445/problem.go
@@ -0,0 +1,24 @@
+package day445
+
+// BinaryTree contains values of either zero (0) or one (1).
+type BinaryTree struct {
+	Value       int
+	Left, Right *BinaryTree
+}
+
+// PruneZeroSubtrees prunes all the subtrees containing only zeros.
+// NOTE: This mutates the input.
+func PruneZeroSubtrees(head *BinaryTree) *BinaryTree {
+	if head == nil {
+		return nil
+	}
+
+	head.Left = PruneZeroSubtrees(head.Left)
+	head.Right = PruneZeroSubtrees(head.Right)
+
+	if head.Value == 0 && head.Left == nil && head.Right == nil {
+		return nil
+	}
+
+	return head
+}
