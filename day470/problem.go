262c58df6da4399508d89990b41bf1d1fd40e648
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2020-03-07 13:17:57 -0700

    day 470: cleaned up day 144

---
+++ day470/problem.go
@@ -0,0 +1,26 @@
+package day470
+
+// ClosestLargerNumberIndexBrute looks for the closest number larger
+// than the target and returns the index of that number.
+// If no number if found, then -1 is returned.
+// Runs in O(N) time.
+func ClosestLargerNumberIndexBrute(nums []int, targetIndex int) int {
+	if targetIndex < 0 || targetIndex >= len(nums) {
+		panic("index out of bounds")
+	}
+
+	val := nums[targetIndex]
+	left, right := targetIndex-1, targetIndex+1
+
+	for left >= 0 || right < len(nums) {
+		if left >= 0 && nums[left] > val {
+			return left
+		} else if right < len(nums) && nums[right] > val {
+			return right
+		}
+		left--
+		right++
+	}
+
+	return -1
+}
---
+++ day470/problem_test.go
@@ -0,0 +1,43 @@
+package day470
+
+import "testing"
+
+// nolint
+var testcases = []struct {
+	nums        []int
+	targetIndex int
+	expected    int
+}{
+	{[]int{4, 1, 3, 5, 6}, 0, 3},
+	{[]int{4, 1, 3, 5, 6}, 4, -1},
+	{[]int{4, 1, 3, 5, 6}, 1, 0},
+}
+
+func TestClosestLargerNumberIndexBrute(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range testcases {
+		if result := ClosestLargerNumberIndexBrute(tc.nums, tc.targetIndex); result != tc.expected {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+
+func TestClosestLargerNumberIndexBruteBoundsCheck(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("It should panic with an index out of bounds")
+		}
+	}()
+	ClosestLargerNumberIndexBrute([]int{1, 2}, 3)
+}
+
+func BenchmarkClosestLargerNumberIndexBrute(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			ClosestLargerNumberIndexBrute(tc.nums, tc.targetIndex)
+		}
+	}
+}
