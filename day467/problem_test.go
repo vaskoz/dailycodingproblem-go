2da84df09ea9ef667d1cccacdc81fd1c64d1bcf3
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2020-03-07 21:24:00 -0700

    day 467: not very interesting

---
+++ day467/problem_test.go
@@ -0,0 +1,30 @@
+package day467
+
+import "testing"
+
+// nolint
+var testcases = []struct {
+	n, expected float64
+}{
+	{9.0, 3.0},
+	{36.0, 6.0},
+	{81.0, 9.0},
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range testcases {
+		if result := Sqrt(tc.n); result != tc.expected {
+			t.Errorf("Expected %v got %v", tc.expected, result)
+		}
+	}
+}
+
+func BenchmarkSqrt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testcases {
+			Sqrt(tc.n)
+		}
+	}
+}
